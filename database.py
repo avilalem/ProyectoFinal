037015fac5e451c7489ebfb97ab5f21b31926358
beto9915 <alcs1491@gmail.com>
2025-10-07 20:09:04 -0600

    Version1

---
+++ database.py
@@ -0,0 +1,57 @@
+import sqlite3 as sql
+from sqlite3 import Connection, Cursor
+
+
+class IDatabaseConnection():
+    def connect(self):
+        raise NotImplementedError
+    def execute(self, query:str, params:tuple=()):
+        raise NotImplementedError
+    def fetchall(self, query:str, params:tuple=()):
+        raise NotImplementedError
+    def close(self):
+        raise NotImplementedError
+
+
+class SQLiteDatabase(IDatabaseConnection):
+    def __init__(self,db_name:str="recetario.db"):
+        self.db_name=db_name
+        self.connection:Connection=None
+        self.connect()
+        self._create_tables()
+    def connect(self):
+        self.connection=sql.connect(self.db_name)
+    def _create_tables(self):
+        cursor=self.connection.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS recetas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        nombre TEXT NOT NULL, 
+        categoria TEXT CHECK(categoria IN('DULCE', 'SALADO')),
+        instrucciones TEXT""")
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS ingredientes(
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        receta_id INTEGER NOT NULL, 
+        nombre TEXT NOT NULL, 
+        cantidad REAL, 
+        unidad TEXT NOT NULL, 
+        FOREIGN KEY(receta_id) REFERENCES recetas(id) ON DELETE CASCADE
+        """)
+        self.connection.commit()
+    def execute(self, query, params=()):
+        cursor:Cursor=self.connection.cursor()
+        cursor.execute(query, params)
+        self.connection.commit()
+        return cursor.lastrowid
+    def fetchall(self, query, params=()):
+        cursor: Cursor = self.connection.cursor()
+        cursor.execute(query, params)
+        self.connection.commit()
+        return cursor.lastrowid
+    def close(self):
+        if self.connection:
+            self.connection.close()
+
+
