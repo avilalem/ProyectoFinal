950352937788914c8ca38314cae6034ada96a3de
beto9915 <alcs1491@gmail.com>
2025-10-11 19:17:09 -0600

    Version1.2

--- models.py
+++ models.py
@@ -6,7 +6,21 @@ class Ingrediente:
         self.nombre=nombre
         self.cantidad=cantidad
         self.unidad=unidad
-    def a_tupla(self, receta_id:int):
+    def to_tuple(self, receta_id:int):
         return(receta_id, self.nombre, self.cantidad,self.unidad)
     def mostrar_ingrediente(self):
         return f"Ingrediente(nombre={self.nombre!r}, cantidad={self.cantidad}, unidad={self.unidad!r}"
+class Receta:
+    def __init__(self, db:SQLiteDatabase, nombre:str, categoria:str, instrucciones:str=""):
+        self.db=db
+        self.id:Optional[int]=None
+        self.nombre=nombre
+        self.categoria=categoria.upper()
+        self.instrucciones=instrucciones
+        self.ingredientes:List[Ingrediente]=Ingrediente
+
+    def agregar_ingrediente(self,nombre:str, cantidad:float, unidad:str):
+        ing=Ingrediente(nombre, cantidad, unidad)
+        self.ingredientes.append(ing)
+    def guardar(self, ):
+
