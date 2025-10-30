abba794e43e2534dd3e189ec9f76521bdbc9f575
beto9915 <alcs1491@gmail.com>
2025-10-29 18:03:05 -0600

    Version1.10

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog
+from PyQt6 import uic
+from database import SQLiteDatabase
+from models import Receta, Ingrediente
+
+class PaginaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("pagina_principal.ui", self)
+        self.db=SQLiteDatabase()
+        self.botonStart.clicked.connect(self.abrir_usuario)
+        self.botonAdmin.clicked.connect(self.abrir_admin)
+
+    def abrir_usuario(self):
+        pass
+    def abrir_admin(self):
+        password, ok=QInputDialog(self, "Acceso administrador", "Ingrese la contraseña:")
+        pass
