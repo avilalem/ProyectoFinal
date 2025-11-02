a09f514ae41d1466d264a3dda636935e5f4bbeef
Ale <avilalejandram@gmail.com>
2025-11-01 19:36:02 -0600

    Merge remote-tracking branch 'origin/BranchAlejandra' into BranchAlejandra

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTextEdit, QHBoxLayout, QPushButton
+from PyQt5.QtCore import Qt
 from PyQt6 import uic
 from database import SQLiteDatabase
 from models import Receta, Ingrediente
+class MessageDialog(QDialog):
+
 
 class PaginaPrincipal(QMainWindow):
     def __init__(self):
@@ -12,10 +16,17 @@ class PaginaPrincipal(QMainWindow):
         self.botonStart.clicked.connect(self.abrir_usuario)
         self.botonAdmin.clicked.connect(self.abrir_admin)
 
+
     def abrir_usuario(self):
         pass
     def abrir_admin(self):
-
+        pass
+class PaginaPrincipalContrasenna:
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("pagina_principal_contraseña.ui", self)
+        self.botonIngresar.clicked.connect(self.abrir_receta)
+        self.botonAdministrar.clicked.connect()
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
