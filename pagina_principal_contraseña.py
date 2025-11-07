0ca0e9154cb163080733dcd16512fa06f1095b5e
beto9915 <alcs1491@gmail.com>
2025-11-06 18:38:18 -0600

    Version 2.4

--- pagina_principal_contraseña.py
+++ pagina_principal_contraseña.py
@@ -18,7 +18,6 @@ class PaginaPassword(QMainWindow):
         self.botonSalir.clicked.connect(self.confirmar_salida)
         self.botonInfo.clicked.connect(lambda: self.open_info("pagina_principal_contraseña"))
         self.botonRegresar.clicked.connect(self.nav.volver_atras)
-        self.botonRegresar.clicked.connect(self.volver)
 
 
     def verificar_contrasena(self):
