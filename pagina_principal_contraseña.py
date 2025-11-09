b376a280692d3f5eaa4815924e4da2c30e6fb9b2
beto9915 <alcs1491@gmail.com>
2025-11-08 23:38:14 -0600

    Version 2.12

--- pagina_principal_contraseña.py
+++ pagina_principal_contraseña.py
@@ -19,6 +19,7 @@ class PaginaPassword(QMainWindow):
         self.botonInfo.clicked.connect(lambda: self.open_info("pagina_principal_contraseña"))
         self.botonRegresar.clicked.connect(self.regresar_a_principal)
         self.linePassword.clear()
+        self.linePassword.returnPressed.connect(self.verificar_contrasena)
         self.linePassword.setFocus()
 
 
